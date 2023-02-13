6f6a9454e9f73eaede2514ef39f81e9b44e0338c
Ricardo Pinto <122636924+ricardoppinto@users.noreply.github.com>
2023-02-13 19:30:53 +0000

    Update Preparing_Dataset.sql

--- Preparing_Dataset.sql
+++ Preparing_Dataset.sql
@@ -4,13 +4,10 @@
 -- Creating the table and importing the main dataset to PostgreSQL
 
 CREATE TABLE demographic_Indicators (
-	ISO3 VARCHAR(50),
-	location VARCHAR(60),
-	year VARCHAR(50),
+	ISO3 VARCHAR(3),
+	location VARCHAR(100),
+	year VARCHAR(4),
 	total_population DECIMAL,
-	male_population DECIMAL,
-	female_population DECIMAL,
-	population_density DECIMAL,
 	sex_ratio DECIMAL,
 	median_age DECIMAL,
 	natural_change_rate DECIMAL,
@@ -19,22 +16,15 @@ CREATE TABLE demographic_Indicators (
 	childbearing_median_age DECIMAL,
 	crude_death_rate DECIMAL,
 	life_expectancy DECIMAL,
-	male_life_expectancy DECIMAL,
-	female_life_expectancy DECIMAL,
-	infant_mortality_rate DECIMAL,
-	net_migration_rate DECIMAL
+	infant_mortality_rate DECIMAL
 
 )
-
 
 COPY demographic_Indicators(
 	ISO3,
 	location,
 	year,
 	total_population,
-	male_population,
-	female_population,
-	population_density,
 	sex_ratio,
 	median_age,
 	natural_change_rate,
@@ -43,10 +33,7 @@ COPY demographic_Indicators(
 	childbearing_median_age,
 	crude_death_rate,
 	life_expectancy,
-	male_life_expectancy,
-	female_life_expectancy,
-	infant_mortality_rate,
-	net_migration_rate)
+	infant_mortality_rate
 
 FROM 'C:\Users\35192\Desktop\Project World Population Evolution\Clean Data\clean_Demographic_Indicators_Data.csv'
 DELIMITER ','
