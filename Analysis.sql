8787341eb1a684e4a5b4c5b676a9aa1e99d34a2a
Ricardo Pinto <122636924+ricardoppinto@users.noreply.github.com>
2023-02-13 22:09:41 +0000

    Create Analysis.sql

---
+++ Analysis.sql
@@ -0,0 +1,55 @@
+/*World Population Data Exploration*/
+
+
+-- World population change through time, with percentage
+
+WITH population_change AS(
+  
+SELECT
+  year,
+	total_population,
+	total_population - LAG(total_population) OVER (ORDER BY year) AS change
+
+FROM 
+  demographic_data_final
+WHERE
+  location = 'World'
+GROUP BY 
+  year, location, total_population
+)
+
+SELECT
+  year,
+  total_population,
+  change/ LAG(total_population) OVER (ORDER BY year) * 100 AS percentage_change
+FROM 
+  population_change
+
+
+
+-- Average percentage change per year
+
+WITH population_change AS(
+
+SELECT
+  year,
+	total_population,
+	total_population - LAG(total_population) OVER (ORDER BY year) AS change
+FROM 
+  demographic_data_final
+WHERE 
+  location = 'World'
+GROUP BY 
+  year, total_population
+)
+
+SELECT 
+  AVG(percentage_change) AS avg_percentage_change
+FROM(
+	SELECT
+	  year,
+    total_population,
+    change/ LAG(total_population) OVER (ORDER BY year) * 100 AS percentage_change
+FROM 
+  population_change
+) sbq
